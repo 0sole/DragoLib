0da068fa3928dc8c28cfd75bea5b4638ee3b6b09
Sole <adcbuyucu@gmail.com>
2026-03-15 01:43:23 +0300

    Update button and toggle functionality in main.lua

--- main.lua
+++ main.lua
@@ -1,21 +1,16 @@
--- Eski hali: local DragoLib = loadfile("Window.lua")()
--- Yeni hali:
+-- main.lua
+-- İnternetten Window.lua'yı çekiyoruz
 local DragoLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/0sole/DragoLib/main/Window.lua"))()
 
--- [[ KULLANIM ]] --
 local Drago = DragoLib:CreateWindow({Name = "DRAGO INTERNAL"})
 
 local Tab1 = Drago:CreateTab("test1")
 local Tab2 = Drago:CreateTab("test")
 
-Tab1:CreateButton("Long Button Example", function() 
-    print("Clicked!") 
+Tab1:CreateButton("Buton Testi", function() 
+    print("Butona basıldı!") 
 end)
 
-Tab1:CreateToggle("Auto Kill", false, function(state) 
-    print("Status:", state) 
-end)
-
-Tab1:CreateKeybind("Aimbot Key", Enum.KeyCode.E, function(key) 
-    print("New Key:", key.Name) 
+Tab1:CreateToggle("Toggle Testi", false, function(state) 
+    print("Durum:", state) 
 end)
