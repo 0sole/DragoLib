616ab828b0457601cae7456bc9780827850cbfde
Sole <adcbuyucu@gmail.com>
2026-03-15 01:46:31 +0300

    Refactor tabs and buttons in main.lua

--- main.lua
+++ main.lua
@@ -1,16 +1,19 @@
--- main.lua
--- İnternetten Window.lua'yı çekiyoruz
+-- Window dosyasını internetten çekiyoruz
 local DragoLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/0sole/DragoLib/main/Window.lua"))()
 
 local Drago = DragoLib:CreateWindow({Name = "DRAGO INTERNAL"})
 
-local Tab1 = Drago:CreateTab("test1")
-local Tab2 = Drago:CreateTab("test")
+local Tab1 = Drago:CreateTab("Main")
+local Tab2 = Drago:CreateTab("Settings")
 
-Tab1:CreateButton("Buton Testi", function() 
-    print("Butona basıldı!") 
+Tab1:CreateButton("Merhaba Drago!", function() 
+    print("Buton çalışıyor!") 
 end)
 
-Tab1:CreateToggle("Toggle Testi", false, function(state) 
-    print("Durum:", state) 
+Tab1:CreateToggle("Örnek Toggle", false, function(state) 
+    print("Toggle:", state) 
+end)
+
+Tab1:CreateKeybind("Menü Tuşu", Enum.KeyCode.RightControl, function(key) 
+    print("Yeni tuş:", key.Name) 
 end)
