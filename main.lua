b5afa39ed9e4ea521dfa729a4bb797237a3f8e8c
Sole <adcbuyucu@gmail.com>
2026-03-15 01:59:49 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -5,6 +5,14 @@ local Drago = DragoLib:CreateWindow({Name = "DRAGO INTERNAL"})
 local Tab1 = Drago:CreateTab("test1")
 local Tab2 = Drago:CreateTab("test")
 
-Tab1:CreateButton("Long Button Example", function() print("Clicked!") end)
-Tab1:CreateToggle("Auto Kill", false, function(state) print("Status:", state) end)
-Tab1:CreateKeybind("Aimbot Key", Enum.KeyCode.E, function(key) print("New Key:", key.Name) end)
+Tab1:CreateButton("Long Button Example", function() 
+    print("Clicked!") 
+end)
+
+Tab1:CreateToggle("Auto Kill", false, function(state) 
+    print("Status:", state) 
+end)
+
+Tab1:CreateKeybind("Aimbot Key", Enum.KeyCode.E, function(key) 
+    print("New Key:", key.Name) 
+end)
